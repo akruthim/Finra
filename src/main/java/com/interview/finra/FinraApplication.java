5f5f4aca192360bd067534009a3c80b2c1e686f0
akruthim <50179897+akruthim@users.noreply.github.com>
2019-05-06 21:36:26 -0400

    Update FinraApplication.java

--- src/main/java/com/interview/finra/FinraApplication.java
+++ src/main/java/com/interview/finra/FinraApplication.java
@@ -1,47 +1,43 @@
 package com.interview.finra;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class FinraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinraApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(FinraApplication.class);
 
-	/**
-	 * Listed few things which i have used in this coding exercise.
-	 * - H2 Db is used to persist for metadata persistence.
-	 * - com.interview.finra.controller.FileController - is the entry point, which has the rest endpoint.
-	 * - Maven is the built tool
-	 * - It's a Multipart file upload. Test file used are in /resources/input
-	 * - Program starts at default 8080 port.
-	 * - JPA style is leverage to interact.
-	 *
-	 * Note: Before you execute the program. Change the property - file.persist.location in application.yml to you suitable location.
-	 *
-	 */
+    public static void main(String[] args) {
+        SpringApplication.run(FinraApplication.class, args);
+    }
 
-	//Curl to upload file. Change the file location according to yours
+    @Value("${file.persist.location}")
+    private String fileUploadDirectory;
 
-	/**
-	 * curl -X PUT \
-	 *   http://localhost:8080/files \
-	 *   -H 'Accept: application/json' \
-	 *   -H 'Content-Type: multipart/form-data' \
-	 *   -H 'content-length: 527' \
-	 *   -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
-	 *   -F file=@/Users/akruthi/Desktop/finra/finance.txt \
-	 *   -F file-metadata=@/Users/akruthi/Desktop/metadata.json
-	 */
-
-
-	//Curl to retrieve metadata
-
-	/**
-	 * curl -X GET \
-	 *   http://localhost:8080/files/3/metadata \
-	 *   -H 'Accept: application/json' \
-	 */
+    /**
+     * This Post construct is a convenient operation i am including it here to create the upload directory.
+     * This creates the upload directory of your choice, so that you need not have to explicitly create one
+     * <p>
+     * As a best practice i have configured upload directory as a property.
+     */
+    @PostConstruct
+    public void verifyUploadDirectory() {
+        if (!Files.exists(Paths.get(fileUploadDirectory))) {
+            try {
+                Files.createDirectory(Paths.get(fileUploadDirectory));
+            } catch (IOException e) {
+                logger.error("Exception occurred while creating upload directory");
+            }
+        }
+    }
 }
