e55f333338b41edd5689bda99c376ca62c88cb1f
akruthim <50179897+akruthim@users.noreply.github.com>
2019-05-06 21:43:23 -0400

    Added new utility

--- src/test/java/com/interview/finra/util/FileUploadTestUtil.java
+++ src/test/java/com/interview/finra/util/FileUploadTestUtil.java
@@ -3,6 +3,8 @@ package com.interview.finra.util;
 import com.interview.finra.converter.FileMetadataToEntityConverter;
 import com.interview.finra.dao.FileMetadataRepository;
 import com.interview.finra.domain.FileMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,15 +15,19 @@ import java.nio.file.Paths;
  */
 public class FileUploadTestUtil {
 
+    private final static Logger logger = LoggerFactory.getLogger(FileUploadTestUtil.class);
     private final static FileMetadataToEntityConverter metadataToEntityConverter = new FileMetadataToEntityConverter();
 
-    public static boolean doesFileExists(String path) {
-        return Files.exists(Paths.get(path));
+    private final static String TEST_UPLOAD_DIRECTORY = Paths.get("").toAbsolutePath().toString() + "/src/test/uploads/";
+    private final static String TEST_UPLOAD_FILE = TEST_UPLOAD_DIRECTORY + "/test-upload.txt";
+
+    public static boolean doesTestFileExists() {
+        return Files.exists(Paths.get(TEST_UPLOAD_FILE));
     }
 
-    public static void deleteFile(String path) {
+    public static void deleteTestFile() {
         try {
-            Files.deleteIfExists(Paths.get(path));
+            Files.deleteIfExists(Paths.get(TEST_UPLOAD_FILE));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -29,5 +35,16 @@ public class FileUploadTestUtil {
 
     public static Long insertTestMetadata(FileMetadataRepository metadataRepository, FileMetadata metadata) {
         return metadataRepository.save(metadataToEntityConverter.convert(metadata)).getId();
+    }
+
+    public static String createTestUploadDirectoryIfDoesntExists() {
+        if (!doesTestFileExists()) {
+            try {
+                Files.createDirectory(Paths.get(TEST_UPLOAD_DIRECTORY));
+            } catch (IOException e) {
+                logger.error("Exception occurred while creating test upload directory");
+            }
+        }
+        return TEST_UPLOAD_DIRECTORY;
     }
 }
